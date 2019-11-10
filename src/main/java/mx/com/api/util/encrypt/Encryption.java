5bb6e9677410e5f2f1aee6d7fd61f08b1a33da0a
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2019-11-10 14:13:05 -0600

    Agrega libreria para genera el sello y el certificado.

---
+++ src/main/java/mx/com/api/util/encrypt/Encryption.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.api.util.encrypt;
+
+import org.apache.commons.ssl.PKCS8Key;
+import javax.security.cert.X509Certificate;
+/**
+ *
+ * @author ignis
+ */
+public class Encryption {
+    
+    public static X509Certificate readCertificate(){
+        try{
+            
+        }catch(Exception error){
+            error.printStackTrace();
+        }
+        
+        return null;
+    }
+}
